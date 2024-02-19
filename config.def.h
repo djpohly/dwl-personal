9d2205cac9990b2bf9446d9d39e679f158887aa9
Devin J. Pohly <djpohly@gmail.com>
2024-02-18 20:48:14 -0600

    Use Rosé Pine base for rootcolor

--- config.def.h
+++ config.def.h
@@ -7,7 +7,7 @@
 static const int sloppyfocus               = 1;  /* focus follows mouse */
 static const int bypass_surface_visibility = 0;  /* 1 means idle inhibitors will disable idle tracking even if it's surface isn't visible  */
 static const unsigned int borderpx         = 1;  /* border pixel of windows */
-static const float rootcolor[]             = COLOR(0x000010ff);
+static const float rootcolor[]             = COLOR(0x191724ff);
 static const float bordercolor[]           = COLOR(0x808080ff);
 static const float focuscolor[]            = COLOR(0xff0000ff);
 static const float urgentcolor[]           = COLOR(0x0000ffff);
