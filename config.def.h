428aeddf89db1ddad1dda4561b968ac5e4c169fc
Devin J. Pohly <djpohly@gmail.com>
2025-12-07 13:23:17 -0600

    use foot in client/server mode

--- config.def.h
+++ config.def.h
@@ -131,7 +131,7 @@ static const enum libinput_config_tap_button_map button_map = LIBINPUT_CONFIG_TA
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
 /* commands */
-static const char *termcmd[] = { "foot", NULL };
+static const char *termcmd[] = { "footclient", NULL };
 static const char *menucmd[] = { "wmenu-run", NULL };
 static const char *colorcmd[] = { "wcolor", NULL };
 static const char *lockcmd[]  = { "wllock", NULL };
